51ba109f2f5f17a23c065ecbe9fe245b6730d70d
“Matt <matt.s.demartino@gmail.com>
2022-11-28 13:11:57 -0500

    Create font and color variables

--- styles/SharedStyles.styled.js
+++ styles/SharedStyles.styled.js
@@ -6,6 +6,16 @@ const GlobalStyles = styled.createGlobalStyle`
     margin: 0;
     padding: 0;
   }
+  :root {
+    --page-font: proxima-nova, sans-serif;
+    --color-blue: #36c5f0;
+    --color-yellow: #ecb22e;
+    --color-green: #2eb67d;
+    --color-red: #e01e51;
+    --color-aubergine: #4a154b;
+    --color-null: #611f69;
+    --color-active: #7c3085;
+  }
 `;
 
 export default GlobalStyles;
