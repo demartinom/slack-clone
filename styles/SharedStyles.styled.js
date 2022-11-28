34427f4ab68587f2179fe71080339e7a8720bac9
“Matt <matt.s.demartino@gmail.com>
2022-11-28 13:12:23 -0500

    Create styling for headers and paragraph elements

--- styles/SharedStyles.styled.js
+++ styles/SharedStyles.styled.js
@@ -16,6 +16,30 @@ const GlobalStyles = styled.createGlobalStyle`
     --color-null: #611f69;
     --color-active: #7c3085;
   }
+  body {
+    font-family: var(--page-font);
+    h1,
+    h2,
+    h3,
+    h4 {
+      font-weight: 700;
+    }
+    h1 {
+      font-size: 36px;
+    }
+    h2 {
+      font-size: 24px;
+    }
+    h3 {
+      font-size: 18px;
+    }
+    h4 {
+      font-size: 12px;
+    }
+    p {
+      font-size: 7px;
+    }
+  }
 `;
 
 export default GlobalStyles;
