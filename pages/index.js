72bd325ee7c8d51cb414286a5f70500e616d3b7d
“Matt <matt.s.demartino@gmail.com>
2022-11-28 13:12:54 -0500

    Create and import NavBar component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 import GlobalStyles from "../styles/SharedStyles.styled";
+import NavBar from "./components/NavBar";
 
 export default function Home() {
-  return <GlobalStyles />;
+  return (
+    <>
+      <GlobalStyles />
+      <NavBar />
+    </>
+  );
 }
