5b0506191e22362068a5fb50e3e6838c383cb34c
“Matt <matt.s.demartino@gmail.com>
2022-11-29 12:18:03 -0500

    Import  and add Slack logo

--- pages/components/NavBar.js
+++ pages/components/NavBar.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { NavBarStyled, Navigation } from "../../styles/NavBar.styled";
+import logo from "../../public/assets/logo.svg";
+import Image from "next/image";
 
 export default function NavBar() {
-  return;
+  return (
+    <NavBarStyled>
+      <Image src={logo} alt="slack logo"></Image>
+      <Navigation>
+        <li>Product</li>
+        <li>Solutions</li>
+        <li>Enterprise</li>
+        <li>Resources</li>
+        <li>Pricing</li>
+      </Navigation>
+    </NavBarStyled>
+  );
 }
