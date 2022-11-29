28641cb623a63f3ed5f7649a3963302f46851795
“Matt <matt.s.demartino@gmail.com>
2022-11-29 12:20:36 -0500

    Add spacing and font weight for li

--- styles/NavBar.styled.js
+++ styles/NavBar.styled.js
@@ -12,9 +12,10 @@ export const NavBarStyled = styled.nav`
 export const Navigation = styled.ul`
   display: flex;
   list-style: none;
+  gap: 1.75rem;
   li {
     color: #fff;
+    font-weight: 700;
     font-size: 0.9rem;
-    padding: 16px 15px;
   }
 `;
