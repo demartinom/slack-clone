7967a3a9bf0e4127ae70ee08d78d3b0a415ddef6
“Matt <matt.s.demartino@gmail.com>
2022-11-29 14:53:30 -0500

    Change to PurpleBackButton

--- pages/components/NavBar.js
+++ pages/components/NavBar.js
@@ -7,23 +7,24 @@ import {
 import logo from "../../public/assets/logo.svg";
 import Image from "next/image";
 import { AiOutlineSearch } from "react-icons/ai";
-import { Button } from "../../styles/SharedStyles.styled";
+import { PurpleBackButton } from "../../styles/SharedStyles.styled";
 
 export default function NavBar() {
   return (
     <NavBarStyled>
-      <Image src={logo} alt="slack logo"></Image>
       <Navigation>
+      <Image src={logo} alt="slack logo"></Image>
         <li>Product</li>
         <li>Solutions</li>
         <li>Enterprise</li>
         <li>Resources</li>
         <li>Pricing</li>
       </Navigation>
+      <ButtonContainer>
       <AiOutlineSearch />
       <h3>Sign In</h3>
-      <ButtonContainer>
-        
+        <PurpleBackButton primary>TALK TO SALES</PurpleBackButton>
+        <PurpleBackButton>TRY FOR FREE</PurpleBackButton>
       </ButtonContainer>
     </NavBarStyled>
   );
