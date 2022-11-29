d936badad5c07af2841472eb333c7a64122ed34d
“Matt <matt.s.demartino@gmail.com>
2022-11-29 15:06:25 -0500

    Import Hero component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import GlobalStyles from "../styles/SharedStyles.styled";
 import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <>
       <GlobalStyles />
       <NavBar />
+      <Hero />
     </>
   );
 }
