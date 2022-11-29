ea14f3ca8e59dbefe26963fa74c7265aa359c311
“Matt <matt.s.demartino@gmail.com>
2022-11-29 15:05:04 -0500

    Create Hero component and style

---
+++ pages/components/Hero.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { HeroStyled } from "../../styles/Hero.styled";
+
+export default function Hero() {
+  return <HeroStyled></HeroStyled>;
+}
---
+++ styles/Hero.styled.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const HeroStyled = styled.div`
+  background-color: var(--color-aubergine);
+`;
