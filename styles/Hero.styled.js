8d3a02dd264b800cb6840e754f6b7a1eadb65878
“Matt <matt.s.demartino@gmail.com>
2022-11-29 15:43:57 -0500

    Hero styling

--- styles/Hero.styled.js
+++ styles/Hero.styled.js
@@ -2,4 +2,24 @@ import styled from "styled-components";
 
 export const HeroStyled = styled.div`
   background-color: var(--color-aubergine);
+  padding-left: 6.5rem;
+  h1 {
+    color: white;
+    max-width: 41rem;
+    font-size: 4rem;
+  }
+  span {
+    color: var(--color-yellow);
+  }
+  P {
+    font-size: 1.25rem;
+    color: white;
+    max-width: 34rem;
+  }
+`;
+
+export const HeroText = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
 `;
