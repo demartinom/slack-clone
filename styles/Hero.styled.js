0970cea62c0cdda4570e7260faa9c6048aec4009
“Matt <matt.s.demartino@gmail.com>
2022-12-19 14:21:22 -0500

    Move dimensions to video styling

--- styles/Hero.styled.js
+++ styles/Hero.styled.js
@@ -30,11 +30,9 @@ export const HeroText = styled.div`
 `;
 
 export const VideoContainer = styled.div`
-  width: 700px;
-  height: 400px;
   video {
-    width: 100%;
-    height: 100%;
+    width: 700px;
+    height: 400px;
   }
 `;
 
