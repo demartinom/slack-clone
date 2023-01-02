f493ce0e5af2114cdc7d0f844871612639921f00
“Matt <matt.s.demartino@gmail.com>
2023-01-02 13:01:56 -0500

    Create array of company logos

---
+++ pages/components/logosArray.js
@@ -0,0 +1,10 @@
+const logos = [
+  { name: "airbnb", image: "/assets/used-by/airbnb-logo.png" },
+  { name: "etsy", image: "/assets/used-by/logo-etsy.png" },
+  { name: "nasa", image: "/assets/used-by/logo-nasa.png" },
+  { name: "nyt", image: "/assets/used-by/nyt.png" },
+  { name: "target", image: "/assets/used-by/target-logo.png" },
+  { name: "uber", image: "/assets/used-by/uber.png" },
+];
+
+export default logos;
