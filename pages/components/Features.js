645bb65c532918402f83e109760505632256a01e
“Matt <matt.s.demartino@gmail.com>
2023-01-02 12:40:51 -0500

    Create Features component

---
+++ pages/components/Features.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { FeaturesStyled } from "../../styles/Features.styled";
+
+export default function Features() {
+  return (
+    <FeaturesStyled>
+      <p>TRUSTED BY COMPANIES ALL OVER THE WORLD</p>
+    </FeaturesStyled>
+  );
+}
