4db7cf640b9dc209ac1c0a2a90081f07df1baa86
“Matt <matt.s.demartino@gmail.com>
2023-01-02 13:02:06 -0500

    Begin component styling

--- styles/Features.styled.js
+++ styles/Features.styled.js
@@ -1,3 +1,21 @@
 import styled from "styled-components";
 
-export const FeaturesStyled = styled.div``;
+export const FeaturesStyled = styled.div`
+  background-color: var(--color-beige);
+`;
+
+export const Companies = styled.div`
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-top: 2rem;
+  p {
+    font-size: 0.875rem;
+    font-weight: 600;
+  }
+`;
+
+export const Logos = styled.div`
+`;
