aeb207361a33f75fc2366a367252fbc9a9c9215e
“Matt <matt.s.demartino@gmail.com>
2023-01-02 13:01:48 -0500

    Map over company logos

--- pages/components/Features.js
+++ pages/components/Features.js
@@ -1,10 +1,23 @@
+import Image from "next/image";
 import React from "react";
-import { FeaturesStyled } from "../../styles/Features.styled";
+import { FeaturesStyled, Companies, Logos } from "../../styles/Features.styled";
+import logos from "./logosArray";
 
 export default function Features() {
+  const companyLogos = logos.map((logo) => (
+    <Image
+      key={logo.name}
+      src={logo.image}
+      fill
+      alt="company logo"
+    ></Image>
+  ));
   return (
     <FeaturesStyled>
-      <p>TRUSTED BY COMPANIES ALL OVER THE WORLD</p>
+      <Companies>
+        <p>TRUSTED BY COMPANIES ALL OVER THE WORLD</p>
+        <Logos>{companyLogos}</Logos>
+      </Companies>
     </FeaturesStyled>
   );
 }
