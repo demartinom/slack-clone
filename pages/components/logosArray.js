5ec5a66ef275352776fa430e1340942084dc02a8
“Matt <matt.s.demartino@gmail.com>
2023-01-02 13:15:18 -0500

    Add height and width to objects

--- pages/components/logosArray.js
+++ pages/components/logosArray.js
@@ -1,10 +1,35 @@
 const logos = [
-  { name: "airbnb", image: "/assets/used-by/airbnb-logo.png" },
-  { name: "etsy", image: "/assets/used-by/logo-etsy.png" },
-  { name: "nasa", image: "/assets/used-by/logo-nasa.png" },
-  { name: "nyt", image: "/assets/used-by/nyt.png" },
-  { name: "target", image: "/assets/used-by/target-logo.png" },
-  { name: "uber", image: "/assets/used-by/uber.png" },
+  {
+    name: "airbnb",
+    image: "/assets/used-by/airbnb-logo.png",
+    width: "112",
+    height: "35",
+  },
+  {
+    name: "etsy",
+    image: "/assets/used-by/logo-etsy.png",
+    width: "74",
+    height: "36",
+  },
+  {
+    name: "nasa",
+    image: "/assets/used-by/logo-nasa.png",
+    width: "67",
+    height: "60",
+  },
+  { name: "nyt", image: "/assets/used-by/nyt.png", width: "233", height: "30" },
+  {
+    name: "target",
+    image: "/assets/used-by/target-logo.png",
+    width: "35",
+    height: "35",
+  },
+  {
+    name: "uber",
+    image: "/assets/used-by/uber.png",
+    width: "75",
+    height: "26",
+  },
 ];
 
 export default logos;
