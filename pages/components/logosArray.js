922d33b624d3d92bd69ef89c910beeb207373474
“Matt <matt.s.demartino@gmail.com>
2023-01-02 13:19:25 -0500

    Correct logo order

--- pages/components/logosArray.js
+++ pages/components/logosArray.js
@@ -6,29 +6,29 @@ const logos = [
     height: "35",
   },
   {
-    name: "etsy",
-    image: "/assets/used-by/logo-etsy.png",
-    width: "74",
-    height: "36",
-  },
-  {
     name: "nasa",
     image: "/assets/used-by/logo-nasa.png",
     width: "67",
     height: "60",
-  },
-  { name: "nyt", image: "/assets/used-by/nyt.png", width: "233", height: "30" },
-  {
-    name: "target",
-    image: "/assets/used-by/target-logo.png",
-    width: "35",
-    height: "35",
   },
   {
     name: "uber",
     image: "/assets/used-by/uber.png",
     width: "75",
     height: "26",
+  },
+  {
+    name: "target",
+    image: "/assets/used-by/target-logo.png",
+    width: "35",
+    height: "35",
+  },
+  { name: "nyt", image: "/assets/used-by/nyt.png", width: "233", height: "30" },
+  {
+    name: "etsy",
+    image: "/assets/used-by/logo-etsy.png",
+    width: "74",
+    height: "36",
   },
 ];
 
