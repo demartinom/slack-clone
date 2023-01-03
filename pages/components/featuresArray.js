f1619098f5cb208e8e068310a4e50ce95de5b0f1
“Matt <matt.s.demartino@gmail.com>
2023-01-03 12:53:43 -0500

    Add id to objects

--- pages/components/featuresArray.js
+++ pages/components/featuresArray.js
@@ -1,17 +1,20 @@
 const featuresArray = [
   {
+    id: 0,
     title: "Bring your team together",
     body: "At the heart of Slack are channels: organized spaces for everyone and everything you need for work. In channels, it’s easier to connect across departments, offices, time zones and even other companies.",
     learnMore: "channels",
     video: "/assets/videos/workChannels.mp4",
   },
   {
+    id: 1,
     title: "Choose how you want to work",
     body: "In Slack, you’ve got all the flexibility to work when, where and how it’s best for you. You can easily chat, send audio and video clips, or hop on a huddle to talk things out live.",
     learnMore: "flexible communication",
     video: "/assets/videos/huddle.mp4",
   },
   {
+    id: 2,
     title: "Move faster with your tools in one place",
     body: "With your other work apps connected to Slack, you can work faster by switching tabs less. And with powerful tools like Workflow Builder, you can automate away routine tasks.",
     learnMore: "the Slack platform",
