10163710a162290d9536626fccf5b9f4ebe1bc4f
“Matt <matt.s.demartino@gmail.com>
2023-01-03 12:53:36 -0500

    Initial creation of features info

--- pages/components/Features.js
+++ pages/components/Features.js
@@ -1,7 +1,15 @@
 import Image from "next/image";
 import React from "react";
-import { FeaturesStyled, Companies, Logos } from "../../styles/Features.styled";
+import {
+  FeaturesStyled,
+  Companies,
+  Logos,
+  FeatureContainer,
+  FeaturesText,
+} from "../../styles/Features.styled";
 import logos from "./logosArray";
+import featuresArray from "./featuresArray";
+import Link from "next/link";
 
 export default function Features() {
   const companyLogos = logos.map((logo) => (
@@ -13,12 +21,34 @@ export default function Features() {
       alt={`${logo.name} logo`}
     ></Image>
   ));
+  const featuresList = featuresArray.map((feature, index) =>
+    index % 2 == 0 ? (
+      <FeatureContainer key={feature.id}>
+        <video loop autoPlay playsInline muted src={feature.video}></video>
+        <FeaturesText>
+          <h1>{feature.title}</h1>
+          <p>{feature.body}</p>
+          <Link href="">Learn more about {feature.learnMore}</Link>
+        </FeaturesText>
+      </FeatureContainer>
+    ) : (
+      <FeatureContainer key={feature.id}>
+        <FeaturesText>
+          <h1>{feature.title}</h1>
+          <p>{feature.body}</p>
+          <Link href="">Learn more about {feature.learnMore}</Link>
+        </FeaturesText>
+        <video loop autoPlay playsInline muted src={feature.video}></video>
+      </FeatureContainer>
+    )
+  );
   return (
     <FeaturesStyled>
       <Companies>
         <p>TRUSTED BY COMPANIES ALL OVER THE WORLD</p>
         <Logos>{companyLogos}</Logos>
       </Companies>
+      {featuresList}
     </FeaturesStyled>
   );
 }
