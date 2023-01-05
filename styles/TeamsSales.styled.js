5f5f83ff6d763da2956e49396300715dcd836db5
“Matt <matt.s.demartino@gmail.com>
2023-01-05 13:52:42 -0500

    Sales styling

---
+++ styles/TeamsSales.styled.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const TeamsSalesStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 5rem 0 0 0;
+  h1 {
+    font-size: 3.125rem;
+  }
+  h3 {
+    font-size: 1.125rem;
+  }
+`;
+
+export const Statistics = styled.div`
+  display: flex;
+  gap: 13rem;
+`;
+
+export const SingleStat = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 12rem;
+  h2 {
+    font-size: 4rem;
+    color: var(--color-aubergine);
+  }
+  p {
+    font-size: 0.875rem;
+  }
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+`;
