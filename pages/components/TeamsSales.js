6112128da41ddb7c6dbb18702e82bc211e44e240
“Matt <matt.s.demartino@gmail.com>
2023-01-05 13:52:37 -0500

    Add body to sales component

--- pages/components/TeamsSales.js
+++ pages/components/TeamsSales.js
@@ -1,5 +1,38 @@
 import React from "react";
+import {
+  TeamsSalesStyled,
+  Statistics,
+  SingleStat,
+  ButtonContainer,
+} from "../../styles/TeamsSales.styled";
+import { Button } from "../../styles/SharedStyles.styled";
 
 export default function TeamsSales() {
-  return <div></div>;
+  return (
+    <TeamsSalesStyled>
+      <h1>Teams large and small rely on Slack</h1>
+      <h3>
+        Slack securely scales up to support collaboration at the world’s biggest
+        companies.
+      </h3>
+      <ButtonContainer>
+        <Button primary>MEET SLACK FOR ENTERPRISE</Button>
+        <Button>TALK TO SALES</Button>
+      </ButtonContainer>
+      <Statistics>
+        <SingleStat>
+          <h2>85%</h2>
+          <p>of users say Slack has improved communication*</p>
+        </SingleStat>
+        <SingleStat>
+          <h2>86%</h2>
+          <p>feel their ability to work remotely has improved*</p>
+        </SingleStat>
+        <SingleStat>
+          <h2>88%</h2>
+          <p>feel more connected to their teams*</p>
+        </SingleStat>
+      </Statistics>
+    </TeamsSalesStyled>
+  );
 }
