788dc3b1a16a1515f4ff2f26272d2764ececef0c
“Matt <matt.s.demartino@gmail.com>
2023-01-06 15:03:47 -0500

    Add data to diveArray

---
+++ pages/components/diveArray.js
@@ -0,0 +1,32 @@
+const diveArray = [
+  {
+    picture: "/assets/deep-dive/collection.png",
+    subject: "Collection",
+    title: "Slack as your digital HQ",
+    linkText: "SEE ALL",
+    purple: true,
+  },
+  {
+    picture: "/assets/deep-dive/resource.png",
+    subject: "Resource",
+    title: "See how others are building their digital HQ",
+    linkText: "READ MORE",
+    purple: false,
+  },
+  {
+    picture: "/assets/deep-dive/webinar.png",
+    subject: "Webinar",
+    title: "Win the battle for talent with a digital HQ",
+    linkText: "WATCH NOW",
+    purple: false,
+  },
+  {
+    picture: "/assets/deep-dive/ebook.png",
+    subject: "E-Book",
+    title: "Reinventing work: New imperatives for the future of working",
+    linkText: "GET E-BOOK",
+    purple: false,
+  },
+];
+
+export default diveArray;
