d20ff1778ff06ac9cedb3b59745e49afc5e73234
“Matt <matt.s.demartino@gmail.com>
2023-01-06 14:39:18 -0500

    Further styling

--- styles/TeamsSales.styled.js
+++ styles/TeamsSales.styled.js
@@ -15,6 +15,9 @@ export const TeamsSalesStyled = styled.div`
     font-weight: 500;
     margin-bottom: 2rem;
   }
+  p {
+    font-size: 0.875rem;
+  }
 `;
 
 export const Statistics = styled.div`
@@ -32,9 +35,6 @@ export const SingleStat = styled.div`
     font-size: 4rem;
     color: var(--color-aubergine);
   }
-  p {
-    font-size: 0.875rem;
-  }
 `;
 
 export const ButtonContainer = styled.div`
@@ -45,6 +45,7 @@ export const ButtonContainer = styled.div`
 
 export const Testimonial = styled.div`
   display: flex;
+  gap: 3rem;
 `;
 
 export const VideoContainer = styled.div`
@@ -70,7 +71,30 @@ export const VideoContainer = styled.div`
 
 export const TestimonialText = styled.div`
   h2 {
-    font-weight: 100;
+    font-weight: 300;
     font-style: italic;
+    font-size: 1.75rem;
+    margin-bottom: 1rem;
   }
+  h4 {
+    font-size: 1rem;
+    span {
+      font-weight: 400;
+    }
+  }
+  a {
+    margin-top: 1rem;
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    font-size: 1.125rem;
+    color: var(--color-link);
+    font-weight: 500;
+  }
+`;
+
+export const Disclaimer = styled.p`
+  margin-top: 3rem;
+  margin-bottom: 2rem;
+  color: grey;
 `;
