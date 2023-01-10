17164e93de9b1865fc985296797bd70e37f7df41
“Matt <matt.s.demartino@gmail.com>
2023-01-10 14:54:09 -0500

    Footer styling

--- styles/Footer.styled.js
+++ styles/Footer.styled.js
@@ -1,9 +1,12 @@
 import styled from "styled-components";
 
 export const FooterStyled = styled.footer`
-  padding: 3rem 6.5rem 0 6.5rem;
+  padding: 3rem 6.5rem 1.5rem 6.5rem;
   img {
     height: 100px;
+  }
+  p {
+    font-size: 0.75rem;
   }
 `;
 
@@ -11,12 +14,13 @@ export const SiteNavigation = styled.div`
   display: flex;
   width: 100%;
   gap: 8rem;
+  margin-bottom: 5rem;
 `;
 
 export const NavigationColumn = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 0.75rem;
+  gap: 1.5rem;
   h2,
   a {
     font-size: 0.875rem;
@@ -42,18 +46,39 @@ export const MetaSocial = styled.div`
   a {
     text-decoration: none;
     color: black;
+    font-size: 0.875rem;
   }
 `;
 
 export const PrivacyLinks = styled.div`
   display: flex;
   align-items: center;
+  gap: 1rem;
+  margin: 1.5rem 0;
   img {
     height: 100%;
   }
   a {
     font-weight: 700;
+    display: flex;
+    align-items: center;
   }
 `;
 
-export const SocialIcons = styled.div``;
+export const SocialIcons = styled.div`
+  display: flex;
+  gap: 1rem;
+  a {
+    display: flex;
+    align-items: center;
+  }
+  p {
+    font-size: 0.875rem;
+    color: var(--color-link);
+    font-weight: 700;
+  }
+  svg {
+    height: 1.2rem;
+    width: 1.2rem;
+  }
+`;
