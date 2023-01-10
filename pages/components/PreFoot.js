90980c56c549b945ef4a0991cfb53bdd0cc9eaae
“Matt <matt.s.demartino@gmail.com>
2023-01-10 13:08:21 -0500

    Import buttons and create PreFootButtons

--- pages/components/PreFoot.js
+++ pages/components/PreFoot.js
@@ -1,6 +1,16 @@
 import React from "react";
 import { PreFootStyled } from "../../styles/PreFoot.styled";
+import { PurpleBackButton } from "../../styles/SharedStyles.styled";
+import { PreFootButtons } from "../../styles/PreFoot.styled";
 
 export default function PreFoot() {
-  return <PreFootStyled></PreFootStyled>;
+  return (
+    <PreFootStyled>
+      <h1>Welcome to your new digital HQ</h1>
+      <PreFootButtons>
+        <PurpleBackButton primary>TALK TO SALES</PurpleBackButton>
+        <PurpleBackButton>TRY FOR FREE</PurpleBackButton>
+      </PreFootButtons>
+    </PreFootStyled>
+  );
 }
--- styles/PreFoot.styled.js
+++ styles/PreFoot.styled.js
@@ -1,3 +1,5 @@
 import styled from "styled-components";
 
 export const PreFootStyled = styled.div``;
+
+export const PreFootButtons = styled.div``;
