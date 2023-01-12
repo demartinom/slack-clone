9e8d8aa1e6f20f4c024fed0302ca5054206767a4
“Matt <matt.s.demartino@gmail.com>
2023-01-12 12:36:11 -0500

    Add link to button

--- pages/components/Hero.js
+++ pages/components/Hero.js
@@ -9,6 +9,7 @@ import {
 import { PurpleBackButton } from "../../styles/SharedStyles.styled";
 import googleLogo from "../../public/assets/google-logo.svg";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Hero() {
   return (
@@ -21,8 +22,10 @@ export default function Hero() {
           With all your people, tools and communication in one place, you can
           work faster and more flexibly than ever before.
         </p>
-        <ButtonContainer >
-          <PurpleBackButton hero>SIGN UP WITH EMAIL</PurpleBackButton>
+        <ButtonContainer>
+          <Link href="https://slack.com/get-started">
+            <PurpleBackButton hero>SIGN UP WITH EMAIL</PurpleBackButton>
+          </Link>
           <GoogleButton hero>
             <Image src={googleLogo} alt="google logo"></Image>SIGN UP WITH
             GOOGLE
