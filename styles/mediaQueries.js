7d4ad9002575ae955c03630bed0493c3e15f0b40
“Matt <matt.s.demartino@gmail.com>
2023-02-03 12:28:49 -0500

    Import media queries

---
+++ styles/mediaQueries.js
@@ -0,0 +1,7 @@
+const sizes = {
+  mobile: "@media (max-width: 425px) ",
+  tablet: "@media (min-width: 426px) and (max-width: 768px) ",
+  computer: "@media (min-width: 769px)",
+};
+
+export default sizes;
