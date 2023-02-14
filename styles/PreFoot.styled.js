e73197b5cd9140ec95ca77bdb6ce0711e6e62f74
“Matt <matt.s.demartino@gmail.com>
2023-02-14 15:06:19 -0500

    Mobile styling

--- styles/PreFoot.styled.js
+++ styles/PreFoot.styled.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { PurpleBackButton } from "./SharedStyles.styled";
+import sizes from "./mediaQueries";
 
 export const PreFootStyled = styled.div`
   background-color: var(--color-aubergine);
@@ -14,14 +15,29 @@ export const PreFootStyled = styled.div`
   h1 {
     font-size: 3.125rem;
   }
+  ${sizes.mobile} {
+    clip-path: ellipse(105% 100% at center top);
+    min-height: 25rem;
+    h1 {
+      font-size: 2rem;
+      text-align: center;
+    }
+  }
 `;
 
 export const PreFootButtonsContainer = styled.div`
-display: flex;
-gap: .5rem;
+  display: flex;
+  gap: 0.5rem;
+  ${sizes.mobile} {
+    flex-direction: column;
+    gap: 0.75rem;
+  }
 `;
 
 export const PreFootButtons = styled(PurpleBackButton)`
   width: 11.4rem;
   height: 3.5rem;
+  ${sizes.mobile} {
+    width: 24rem;
+  }
 `;
